504481313140d40c3a62ae1b71658f09acceb615
Jaime Andres Valencia <jgaimer42@gmail.com>
2022-08-14 23:51:15 -0500

    little bit code cleaning

--- multiplicacion_matriz.c
+++ multiplicacion_matriz.c
@@ -6,52 +6,50 @@ int user_input(){
     int size=0;
     printf("Ingrese el tamaño de las matricez: ");
     scanf("%d",&size);
+    printf("\n");
     return size;
+}
+
+void show_matrix(int size, int matrix[size][size], char name[]){
+    printf("Showing %s \n",name);
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 int main(void) {
     int size = user_input();
-    int matrizA[size][size]; 
-    int matrizB[size][size];
-    int producto[size][size];
-    int col,row = 0;
-    memset(matrizA,0,sizeof(matrizA));
-    while(row < size){
-        col = 0;
-        while(col < size){
-            matrizA[col][row] = rand()%100;
-            col ++;
+    int matrix_a[size][size],matrix_b[size][size],result[size][size];
+    
+    memset(matrix_a,0,sizeof(matrix_a));
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++){
+            matrix_a[col][row] = rand()%100;
         }
-        row ++;
     }
+    show_matrix(size,matrix_a,"A");
 
-    memset(matrizB,0,sizeof(matrizB));
-    row = 0;
-    col = 0;
-    while(row < size){
-        col = 0;
-        while(col < size){
-            matrizB[col][row] = rand()%100;
-            col ++;
+    memset(matrix_b,0,sizeof(matrix_b));
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++){
+            matrix_b[col][row] = rand()%100;
         }
-        row ++;
     }
-    memset(producto,0,sizeof(producto));
+    show_matrix(size,matrix_b,"B");
 
+    memset(result,0,sizeof(result));
     for (int a = 0; a < size; a++) {
         for (int i = 0; i < size; i++) {
             int suma = 0;
             for (int j = 0; j < size; j++) {
-                suma += matrizA[i][j] * matrizB[j][a];
+                suma += matrix_a[i][j] * matrix_b[j][a];
             }
-            producto[i][a] = suma;
+            result[i][a] = suma;
         }
     }
-    printf("Imprimiendo producto\n");
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            printf("%d ", producto[i][j]);
-        }
-        printf("\n");
-    }
+    show_matrix(size,result,"result");
 }
