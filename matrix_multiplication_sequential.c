a693ea3baa2ecb13682258a5d742c3f11f2617f5
Jaime Andres Valencia <jgaimer42@gmail.com>
2022-08-17 23:34:51 -0500

    first thread addvance

--- matrix_multiplication_sequential.c
+++ matrix_multiplication_sequential.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-int user_input(){
-    int size=0;
-    printf("Ingrese el tamaño de las matricez: ");
-    scanf("%d",&size);
-    printf("\n");
-    return size;
-}
+#include <time.h>
 
 void show_matrix(int size, long **matrix, char name[]){
-    printf("Showing %s \n",name);
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+    printf("Showing %s \n", name);
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
             printf("%ld\t", matrix[i][j]);
         }
         printf("\n");
@@ -21,32 +14,40 @@ void show_matrix(int size, long **matrix, char name[]){
     printf("\n");
 }
 
-
 void fill_matrix(int size, long **matrix){
-  for (int row = 0; row < size; row++) {
+    for (int row = 0; row < size; row++){
         for (int col = 0; col < size; col++){
-            matrix[col][row] = rand()%100;
+            matrix[col][row] = rand() % 100;
         }
-    }  
-}
-
-void assign_memory_to_matrix(int size,long **matrix){
-    for (int index=0;index<size;++index){
-        matrix[index] = (long*)malloc(size * sizeof(long));
     }
 }
 
-int main(void) {
-    int size = user_input();
-    long **result,**matrix_a,**matrix_b;
+void assign_memory_to_matrix(int size, long **matrix){
+    for (int index = 0; index < size; ++index){
+        matrix[index] = (long *)malloc(size * sizeof(long));
+    }
+}
+
+int main(int argc, char *argv[]){
+    int size;
+    sscanf(argv[1], "%d", &size);
+    long **result, **matrix_a, **matrix_b;
+    time_t t;
+    srand((unsigned)time(&t));
+
     matrix_a = (long**)malloc(size*sizeof(long*));
     assign_memory_to_matrix(size,matrix_a);
     fill_matrix(size,matrix_a);
+
     matrix_b = (long**)malloc(size*sizeof(long*));
     assign_memory_to_matrix(size,matrix_b);
     fill_matrix(size,matrix_b);
+
     result = (long**)malloc(size*sizeof(long*));
     assign_memory_to_matrix(size,result);
+    clock_t start, end;
+    double cpu_time_used;
+    start = clock();
     for (int a = 0; a < size; a++) {
         for (int i = 0; i < size; i++) {
             long suma = 0;
@@ -56,5 +57,7 @@ int main(void) {
             result[i][a] = suma;
         }
     }
-    show_matrix(size,result,"result");
+    end = clock();
+    cpu_time_used = end -start;
+    printf("%lf\n",cpu_time_used);
 }
