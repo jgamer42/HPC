da83b3bbc1a376cec3dadb6695339460d1ef7b39
Jaime Andres Valencia <jgaimer42@gmail.com>
2022-08-19 15:17:05 -0500

    ready to test

---
+++ benchamark.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+for i in 1000 1500 2000 2500 5000 6000 7000 8000 10000
+do
+echo "running sequential"
+./sequential $i >> sequential.txt 
+done
+
+for j in 2 4 6 8 16
+do
+for i in 1000 1500 2000 2500 5000 6000 7000 8000 10000
+do
+echo "running threads $j"
+./threads $i $j >> $j.txt 
+done
+done
---
+++ matrix_multiplication_threads.c
@@ -0,0 +1,112 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+long **result, **matrix_a, **matrix_b;
+struct thread_data
+{
+    int start_col;
+    int end_col;
+    int start_row;
+    int end_row;
+    int thread_id;
+};
+
+void *multiply_matrix(void *threadid)
+{
+    struct thread_data *data;
+    int start_row,start_col,end_col,end_row,id;
+    data = (struct thread_data *)threadid;
+    start_row = data->start_row;
+    start_col = data->start_col;
+    end_row = data->end_row;
+    end_col = data->end_col;
+    id = data->thread_id;
+    for (int a = 0; a < end_col; a++) {
+        for (int i = 0; i < end_row; i++) {
+            long suma = 0;
+            for (int j = 0; j < end_col; j++) {
+                suma += matrix_a[i][j] * matrix_b[j][a];
+            }
+            result[i][a] = suma;
+        }
+    }
+    pthread_exit(NULL);
+}
+
+
+void fill_matrix(int size, long **matrix){
+    for (int row = 0; row < size; row++){
+        for (int col = 0; col < size; col++){
+            matrix[col][row] = rand() % 100;
+        }
+    }
+}
+
+void assign_memory_to_matrix(int size, long **matrix){
+    for (int index = 0; index < size; ++index){
+        matrix[index] = (long *)malloc(size * sizeof(long));
+    }
+}
+
+void show_matrix(int size, long **matrix, char name[]){
+    printf("Showing %s \n", name);
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            printf("%ld\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void main(int argc, char *argv[])
+{
+    int rc,size,n_threads,actual_block = 0;
+    scanf(argv[2],"%d",&n_threads);
+    sscanf(argv[1], "%d", &size);
+    pthread_t threads[n_threads];
+    void *status;
+    int charge_by_thread = size/n_threads;
+    matrix_a = (long**)malloc(size*sizeof(long*));
+    assign_memory_to_matrix(size,matrix_a);
+    fill_matrix(size,matrix_a);
+    matrix_b = (long**)malloc(size*sizeof(long*));
+    assign_memory_to_matrix(size,matrix_b);
+    fill_matrix(size,matrix_b);
+    result = (long**)malloc(size*sizeof(long*));
+    assign_memory_to_matrix(size,result);
+    clock_t start, end;
+    double cpu_time_used;
+    start = clock();
+    for (int t = 0; t < n_threads; t++)
+    {
+        struct thread_data data;
+        data.start_col = 0;
+        data.end_col = size;
+        data.start_row = actual_block;
+        if((actual_block + charge_by_thread) >= size){
+            data.end_row = size;
+        }else{
+            data.end_row = actual_block + charge_by_thread;
+        }
+        actual_block = actual_block + charge_by_thread;
+        rc = pthread_create(&threads[t], NULL, multiply_matrix, (void *)&data);
+        if (rc)
+        {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+    }
+    for(int t=0; t<n_threads; t++) {
+       rc = pthread_join(threads[t], &status);
+       if (rc) {
+          printf("ERROR; return code from pthread_join() is %d\n", rc);
+          exit(-1);
+          }
+    }
+    end = clock();
+    cpu_time_used = end -start;
+    printf("%lf\n",cpu_time_used);
+    pthread_exit(NULL);
+}
