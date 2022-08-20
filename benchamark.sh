c08f2c9d32980f543ee07defef3982638dd853ab
Jaime Andres Valencia <jgaimer42@gmail.com>
2022-08-20 01:11:35 -0500

    fix benchmark

--- benchamark.sh
+++ benchamark.sh
@@ -1,15 +1,13 @@
 #! /bin/bash
-for i in 1000 1500 2000 2500 5000 6000 7000 8000 10000
-do
-echo "running sequential"
-./sequential $i >> sequential.txt 
-done
-
-for j in 2 4 6 8 16
-do
-for i in 1000 1500 2000 2500 5000 6000 7000 8000 10000
-do
-echo "running threads $j"
-./$j $i >> $j.txt 
-done
+for k {1..10}
+    for i in 1000 1500 2000 2500 5000 6000 7000 8000 10000
+    do
+        echo "iteration $k" >> sequential.txt
+        ./sequential $i >> sequential.txt 
+        for j in 2 4 6 8 16
+        do
+            echo "iteration $k" >> $k.txt
+            ./$j $i >> $j.txt
+        done
+    done
 done
