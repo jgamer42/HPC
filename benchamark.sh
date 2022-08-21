907c597c54c0478c607f23a933e0ec66b39b059a
Jaime Andres Valencia <jgaimer42@gmail.com>
2022-08-20 20:37:34 -0500

    fix benchmark

--- benchamark.sh
+++ benchamark.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 for k in {1..10}
 do
-    for i in 100 200 300
+    for i in 1000 1500 2000 2500 5000 6000 7000 8000 10000
     do
         echo "iteration $k $i" >> sequential.txt
         ./sequential $i >> sequential.txt 
