8d5a18a24ad688d8dc0cdfc25ee9d135df46f444
Jaime Andres Valencia <jgaimer42@gmail.com>
2022-09-21 23:44:05 -0500

    challenge

--- reto/benchmark.sh
+++ reto/benchmark.sh
@@ -5,11 +5,15 @@ do
     do
         for steps in 100 200 250
         do
-            echo "iteracion $k"
-            ./process $i $j >> process.txt
+            echo "iteracion $k $steps $size" >> sequential.txt
+            ./sequential $steps $size >> sequential.txt
             for threads in 2 4 6
-
             do
+                echo "iteracion $k $steps $size $threads" >> process.txt
+                ./process $steps $size $threads >> process.txt
+                echo "iteracion $k $steps $size $threads" >> threads.txt
+                ./threads $steps $size $threads >> process.txt
+            done  
         done
     done
 done
